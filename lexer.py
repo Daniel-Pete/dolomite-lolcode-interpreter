dd5644df7517c36f8cba9fba5c4a6b41e34fe310
stevenacrespo <sacrespo@up.edu.ph>
2020-11-13 17:08:00 +0800

    Add Lexer File

---
+++ lexer.py
@@ -0,0 +1,33 @@
+# Branch David
+# Lexical Analyser
+# Nov 13 2020
+
+import re
+
+file = "data/sample.txt"
+
+r_numbr = "^-?[0-9]*$"
+r_varident = "^[a-zA-Z][a-zA-Z0-9\_]$"
+
+
+
+def tokenize(fn):
+
+    f = open(fn, "r")
+
+    for line in f:
+        for elem in line.split():
+            if re.match(r_numbr, elem):
+                print("NUMBR Identifier:", elem)
+            if re.match(r_varident, elem):
+                print("Variable Identifier:",elem)
+
+
+
+
+def main():
+
+    tokenize(file)
+
+
+main()
